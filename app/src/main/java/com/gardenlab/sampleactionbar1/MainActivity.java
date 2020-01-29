09fc0ca660aa17d565661699897c0e6c8fb138fe
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-29 15:31:15 +0900

    Show Home Icon

--- app/src/main/java/com/gardenlab/sampleactionbar1/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampleactionbar1/MainActivity.java
@@ -1,14 +1,61 @@
 package com.gardenlab.sampleactionbar1;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBar abar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        abar = getSupportActionBar();
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abar.setLogo(R.drawable.home);
+                abar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME|ActionBar.DISPLAY_USE_LOGO);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int curId = item.getItemId();
+
+        switch (curId) {
+            case R.id.menu_refresh:
+                Toast.makeText(this,"새로고침 메뉴 선택", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_search:
+                Toast.makeText(this,"검색 메뉴 선택", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_settings:
+                Toast.makeText(this,"설정 메뉴 선택", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
